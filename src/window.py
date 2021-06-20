94dae930462839275e77953ee0c2009b35cdedd4
Arthur M R Alves <artmr.alves@gmail.com>
2021-06-20 17:55:21 -0300

    Menu para realização de zoom in/out criado.

--- src/window.py
+++ src/window.py
@@ -5,43 +5,18 @@ from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QGridLayout, QVBoxLayo
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.resize(1000,800)
+        self.resize(1000, 800)
         self.setWindowTitle('Trabalho CG - Arthur Moreira R Alves & Bryan Martins Lima')
         # TODO: colocar icone pra ficar bonito
         # TODO: colocar pra abrir no centro da tela
 
-
         # adiciona os componentes à janela principal
         layout = QGridLayout()
-        funcMenu = self.createFuncMenu()
+        funcMenu = createFuncMenu()
         funcMenu.setParent(self)
         layout.addWidget(funcMenu, 0, 0)
         self.show()
 
-
-    # metodo responsável por criar o menu que contém as funcionalidades da aplicação
-    def createFuncMenu(self) -> QWidget:
-        funcMenu = QWidget()
-        layout = QVBoxLayout()
-        movMenu = self.createMovementMenu()
-        movMenu.setParent(funcMenu)
-        layout.addWidget(movMenu)
-        funcMenu.setLayout(layout)
-        return funcMenu
-
-    # metodo responsável por criar o menu que contém as funcionalidades de movimentação no viewport
-    def createMovementMenu(self) -> QWidget:
-        movMenu = QWidget()
-        layout = QGridLayout()
-        label = QLabel('Movimentação')
-        label.setAlignment(QtCore.Qt.AlignCenter)
-        layout.addWidget(label, 0, 0, 1, 3)
-        layout.addWidget(QPushButton('Up'), 1, 1)
-        layout.addWidget(QPushButton('Left'), 2, 0)
-        layout.addWidget(QPushButton('Right'), 2, 2)
-        layout.addWidget(QPushButton('Down'), 3, 1)
-        movMenu.setLayout(layout)
-        return movMenu
 
     # TODO: Criar menu de zoon in/ zoon out
     # TODO: Criar menu que mostra os objs
@@ -52,3 +27,45 @@ class MainWindow(QWidget):
     # TODO: implementar funcionalidade de zoom in/out
     # TODO: implementar funcionalidade de mover esq/dir/cima/baixo
     # TODO: implementar a transformada lá
+
+
+# metodo responsável por criar o menu que contém as funcionalidades da aplicação
+def createFuncMenu() -> QWidget:
+    funcMenu = QWidget()
+    layout = QVBoxLayout()
+    movMenu = createMovementMenu()
+    movMenu.setParent(funcMenu)
+    layout.addWidget(movMenu)
+    zoomMenu = createZoomMenu()
+    zoomMenu.setParent(funcMenu)
+    layout.addWidget(zoomMenu)
+    funcMenu.setLayout(layout)
+    return funcMenu
+
+
+# metodo responsável por criar o menu que contém as funcionalidades de movimentação no viewport
+def createMovementMenu() -> QWidget:
+    movMenu = QWidget()
+    layout = QGridLayout()
+    label = QLabel('Movimentação')
+    label.setAlignment(QtCore.Qt.AlignCenter)
+    layout.addWidget(label, 0, 0, 1, 3)
+    layout.addWidget(QPushButton('Up'), 1, 1)
+    layout.addWidget(QPushButton('Left'), 2, 0)
+    layout.addWidget(QPushButton('Right'), 2, 2)
+    layout.addWidget(QPushButton('Down'), 3, 1)
+    movMenu.setLayout(layout)
+    return movMenu
+
+
+# função responsável por criar o menu de zoom in/out
+def createZoomMenu() -> QWidget:
+    zoomMenu = QWidget()
+    layout = QGridLayout()
+    label = QLabel('Zoom')
+    label.setAlignment(QtCore.Qt.AlignCenter)
+    layout.addWidget(label, 0, 0, 1, 2)
+    layout.addWidget(QPushButton('in'), 1, 0)
+    layout.addWidget(QPushButton('out'), 1, 1)
+    zoomMenu.setLayout(layout)
+    return zoomMenu
